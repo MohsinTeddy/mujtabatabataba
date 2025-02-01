aaea537e835495479440f4402b5111ea417a3a57
Mujtaba <mujtabamustafa2909@gmail.com>
2025-02-01 14:07:55 +0300

    IDK

---
+++ mohsin.py
@@ -0,0 +1,5 @@
+import random
+mehboob = input("Type Mehboob in a funny way with creativity .......")
+
+for i in range(len(mehboob) + random.randint(20,90)):
+    print(f"O mere {mehboob + "a"} oo mere {mehboob}.")
